ed88a7132afc05e9d5849261873240abc6e4c3b0
Ewoma Akwenuke <ewomaakwenuke@gmail.com>
2024-11-02 22:46:03 +0100

    Set up Express server with MongoDB connection; added URL shortening schema and basic routes for creating and viewing short URLs

---
+++ models/shortUrl.js
@@ -0,0 +1,20 @@
+const { mongo } = require("mongoose");
+
+const shortUrlSchema = new mongoose.Scheme({
+    full: {
+        type: String,
+        required: true
+    },
+    short: {
+        type: String,
+        required: true,
+        default: shortId.generate
+    },
+    clicks: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+})
+
+module.exports = mongoose.model('shortUrl', shortUrlSchema)
