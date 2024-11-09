474c75ec1d8b85aa9a789a54ed900153ca42967c
Ewoma Akwenuke <ewomaakwenuke@gmail.com>
2024-11-09 23:48:05 +0100

    Added URL shortening schema and basic routes for creating and viewing short URLs

--- models/shortUrl.js
+++ models/shortUrl.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const shortId = require("shortid")
 
+
 const shortUrlSchema = new mongoose.Scheme({
     full: {
         type: String,
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
-// const mongoose = require('mongoose')
-// const ShortUrl = require('./models/shortUrl')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
 const app = express()
 
-// mongoose.connect('mongodb://localhost/urlShortener', {
-//    useNewUrlParser: true, useUnifiedTopology: true
-// })
+mongoose.connect('mongodb://localhost/urlShortener', {
+   useNewUrlParser: true, useUnifiedTopology: true
+})
 
 
 app.set('view engine', 'ejs')
@@ -17,21 +17,21 @@ app.get('/' , async (req, res) => {
 
 })
 
-app.post('shortUrls',  async (req, res) => {
+app.post('/shortUrls',  async (req, res) => {
  await ShortUrl.create({full: req.body.fullUrl})
  res.redirect('/')
 
 })
 
-app.get('/:shortUrl', async (req, res) => {
-  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
-  if (shortUrl == null) return res.sendStatus(404)
+// app.get('/:shortUrl', async (req, res) => {
+//   const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+//   if (shortUrl == null) return res.sendStatus(404)
 
-  shortUrl.clicks++
-  shortUrl.save()
+//   shortUrl.clicks++
+//   shortUrl.save()
 
-  res.redirect(shortUrl.full)
-})
+//   res.redirect(shortUrl.full)
+// })
 
 
 app.listen(process.env.PORT || 5000)
